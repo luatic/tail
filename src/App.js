26e98f4973564bfbbcfbaf8cf2f0b51a583f3f4c
Patrycjusz Myszka <pat.myszka@gmail.com>
2023-02-19 15:08:47 +0000

    cos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1 className="relative h-32 w-32 font-bold ">
+      <div className="absolute inset-x-0 top-0 h-16">cos tam chujku</div>
+    </h1>
+  )
 }
-
-export default App;
