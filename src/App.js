f857806637be6d3d110f3883012355d3fcc1417c
Patrycjusz Myszka <pat.myszka@gmail.com>
2023-02-19 16:25:51 +0000

    jjjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <h1 className="relative h-32 w-32 font-bold ">
-      <div className="absolute inset-x-0 top-0 h-16">cos tam chujku</div>
+    <h1 className="text-center text-green-500 font-bold">
+      jak sie masz chujku
     </h1>
   )
 }
